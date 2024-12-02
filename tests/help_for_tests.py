410fc7da385ea0ac8b2de9f0f8ef5b752652d322
Alex Iva <alex.i@aktru.video>
2024-12-02 22:19:45 +0300

    Изменения в рамках ревью №3 Изменены ассерты в тесты Добавлен файл help_for_tests.py с УРЛ сайта Добавлена информация в README.md

--- tests/help_for_tests.py
+++ tests/help_for_tests.py
@@ -2,23 +2,3 @@
 GET_MAIN_URL = 'https://stellarburgers.nomoreparties.site/'  # URL стартовой страницы сайта Stellar Burgers
 GET_REG_URL = 'https://stellarburgers.nomoreparties.site/register'  # URL страницы регистрации сайта Stellar Burgers
 GET_RECPASS_URL = 'https://stellarburgers.nomoreparties.site/forgot-password'  # URL страницы восстановления пароля сайта Stellar Burgers
-
-# driver.find_element(*LocatorsMainPage.SEARCH_ORDER_BUTTON_TOP).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_NAME_FIELD).send_keys(name)
-        # driver.find_element(*LocatorsOrderPage.SEARCH_SURNAME_FIELD).send_keys(surname)
-        # driver.find_element(*LocatorsOrderPage.SEARCH_CITY_FIELD).send_keys(city)
-        # driver.find_element(*LocatorsOrderPage.SEARCH_METRO_FIELD).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_METRO_BUTTON).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_PHONE_NUMBER_FIELD).send_keys(phone)
-        # driver.find_element(*LocatorsOrderPage.SEARCH_CONTINUE_BUTTON).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_DATA_FIELD).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_DAY_BUTTON).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_RENT_FIELD).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_COUNT1_RENT_BUTTON).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_COLOUR1_FIELD).click()
-        # driver.find_element(*LocatorsOrderPage.SEARCH_ORDER_BUTTON).click()
-        # WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LocatorsOrderPage.SEARCH_YESORNO_FIELD))
-        # driver.find_element(*LocatorsOrderPage.SEARCH_YES_BUTTON).click()
-        # WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(LocatorsOrderPage.SEARCH_ORDER_INFO_FIELD))
-        # driver.find_element(*LocatorsOrderPage.SEARCH_STATUS_BUTTON).click()
-
